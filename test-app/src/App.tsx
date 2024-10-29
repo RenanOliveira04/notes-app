1dd74008c94efcf183c0425664dbe62515855e21
Renan Oliveira <rlsorenan@gmail.com>
2024-10-29 20:59:36 -0300

    Refactor TodoItems component and add TodoItem and TodoList components
    - Refactor the TodoItems component to TodoItem component and move it to a separate file
    - Add TodoList component to display a list of todos
    - Update App component to use the new TodoItem and TodoList components
    - Update imports and references in the code accordingly

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,36 +1,48 @@
-  import { useState } from "react"
-  import TodoItems from "./components/TodoItems"
-  import { dummyData } from "./data/todos"
+import { useState } from "react"
+import TodoItem from "./components/TodoItem"
+import { dummyData } from "./data/todos"
 import AddTodoForm from "./components/AddTodoForm"
+import TodoList from "./components/TodoList"
 
-  function App() {
+function App() {
 
-    const [todos, setTodos] = useState(dummyData)
+  const [todos, setTodos] = useState(dummyData)
 
-    function setTodoCompleted(id: number, completed: boolean) {
-      setTodos((prevTodos) => 
-      prevTodos.map((todo) => 
-      todo.id === id ? 
-      {...todo, completed} : todo))
-    }
-
-    return (
-      <main className="py-10 h-screen space-y-5">
-        <h1 className="text-3xl font-bold text-center">Your Todos</h1>
-        <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-5">
-          <AddTodoForm />
-          <div className="space-y-2">
-            {todos.map((todo) => (
-              <TodoItems
-              key={todo.id} 
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-             />
-            ))}
-          </div>
-        </div>
-      </main>
-    )
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ?
+          { ...todo, completed } : todo))
   }
 
-  export default App
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: Math.max(...prevTodos.map((todo) => todo.id), 0) + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ]);
+  }
+
+  function deleteTodo (id: number) {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
+  }
+
+  return (
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="text-3xl font-bold text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-5">
+        <AddTodoForm onSubmit={addTodo}/>
+        <TodoList 
+        todos={todos} 
+        onCompletedChange={setTodoCompleted}
+        onDelete={deleteTodo}
+        />
+      </div>
+    </main>
+  )
+}
+
+export default App
